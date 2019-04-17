79529dce75964f3771ce17647fb25ed7cd0e0739
Nikhil Patil <nikhil.patil@in.abb.com>
2019-04-17 13:26:17 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+  public componentDidMount = () =>{
+    // call myab api
+  }
+
+  public render() {
+    return (
+      <div className="App">
+        Coming soon...
+      </div>
+    );
+  }
+}
+
+export default App;
